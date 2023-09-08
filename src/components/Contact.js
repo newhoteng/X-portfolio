fbe57eca1e1d82230f1a914ad17bb868d296dc4c
Harriet Oteng <harriet.oteng@gmail.com>
2023-09-08 19:29:28 +0000

    Add styling to contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -10,40 +10,48 @@ function Contact() {
   return (
     <div className="contact-container hc">
       <h1>CONTACT</h1>
-      <div className="email-container">
-        <p className="email-item">EMAIL</p>
-        <div className="email-item">
-          <a href="mailto:harriet.oteng@gmail.com">harriet.oteng@gmail.com</a>
-          <a href="mailto:harriet.oteng@yahoo.com">harriet.oteng@yahoo.com</a>
+      <div className="details">
+        <div className="email-container">
+          <p className="contact-heading">EMAIL</p>
+          <div className="contact-body">
+            <a className="social-link" href="mailto:harriet.oteng@gmail.com">
+              <FiArrowUpRight />
+              harriet.oteng@gmail.com
+            </a>
+            <a className="social-link" href="mailto:harriet.oteng@yahoo.com">
+              <FiArrowUpRight />
+              harriet.oteng@yahoo.com
+            </a>
+          </div>
         </div>
-      </div>
-      <div className="socials-container">
-        <p className="socials-item">SOCIAL MEDIA</p>
-        <div className="socials-item">
-          <Link to="https://www.linkedin.com/in/harriet-oteng/" target="_blank" rel="noreferrer">
-            <div className="social-link">
-              <FiArrowUpRight />
-              <p>LinkedIn</p>
-            </div>
-          </Link>
-          <Link to="https://github.com/newhoteng" target="_blank" rel="noreferrer">
-            <div className="social-link">
-              <FiArrowUpRight />
-              <p>GitHub</p>
-            </div>
-          </Link>
-          <Link to="https://instagram.com/harriet.oteng?igshid=OGQ5ZDc2ODk2ZA==" target="_blank" rel="noreferrer">
-            <div className="social-link">
-              <FiArrowUpRight />
-              <p>Instagram</p>
-            </div>
-          </Link>
-          <Link to="https://twitter.com/HarrietOteng1" target="_blank" rel="noreferrer">
-            <div className="social-link">
-              <FiArrowUpRight />
-              <p>Twitter</p>
-            </div>
-          </Link>
+        <div className="socials-container">
+          <p className="contact-heading">SOCIAL MEDIA</p>
+          <div className="contact-body">
+            <Link to="https://www.linkedin.com/in/harriet-oteng/" target="_blank" rel="noreferrer">
+              <div className="social-link">
+                <FiArrowUpRight />
+                <p>LinkedIn</p>
+              </div>
+            </Link>
+            <Link to="https://github.com/newhoteng" target="_blank" rel="noreferrer">
+              <div className="social-link">
+                <FiArrowUpRight />
+                <p>GitHub</p>
+              </div>
+            </Link>
+            <Link to="https://instagram.com/harriet.oteng?igshid=OGQ5ZDc2ODk2ZA==" target="_blank" rel="noreferrer">
+              <div className="social-link">
+                <FiArrowUpRight />
+                <p>Instagram</p>
+              </div>
+            </Link>
+            <Link to="https://twitter.com/HarrietOteng1" target="_blank" rel="noreferrer">
+              <div className="social-link">
+                <FiArrowUpRight />
+                <p>Twitter</p>
+              </div>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
