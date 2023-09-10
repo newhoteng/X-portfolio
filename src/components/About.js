7b2adecdee0c32a4f5c3e730b722d43fa3b15e94
Harriet Oteng <harriet.oteng@gmail.com>
2023-09-10 23:38:51 +0000

    Add profile image

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import profilePic from '../images/HT2-cropped.jpg';
 
 function About() {
   return (
-    <h1>This is the about page</h1>
+    <div>
+      <div><img src={profilePic} alt="Harriet in a black dress" /></div>
+      <h1>This is the about page</h1>
+    </div>
   );
 }
 
