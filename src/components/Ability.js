f88392ce4ee06d18a5a3ab3b2151d34f86c3bc49
Harriet Oteng <harriet.oteng@gmail.com>
2023-09-14 14:12:36 +0000

    Add collapsible feature for skills

--- src/components/Ability.js
+++ src/components/Ability.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+// import { useState } from 'react';
 import PropTypes from 'prop-types';
 import { FiArrowRight } from 'react-icons/fi';
 
 function DropDown({ ability }) {
+  const [arrowIconClass, setarrowIcon] = useState('');
+  const [skillDetailsClass, setskillDetails] = useState('details hidden');
+
+  const handleArrowClick = () => {
+    if (skillDetailsClass === 'details hidden') {
+      setskillDetails('details visible');
+      setarrowIcon('rotate');
+    } else {
+      setskillDetails('details hidden');
+      setarrowIcon('');
+    }
+  };
+
   return (
     <div className="language-cont">
-      <h2>
+      <h2 onClick={handleArrowClick} role="presentation">
         {ability.type}
-        <FiArrowRight />
+        <FiArrowRight className={arrowIconClass} />
       </h2>
-      <div>
+      <div className={skillDetailsClass}>
         {ability.list.map((item) => (
           <p key={item}>{item}</p>
         ))}
@@ -22,7 +36,6 @@ DropDown.propTypes = {
   ability: PropTypes.shape({
     type: PropTypes.string,
     list: PropTypes.arrayOf(PropTypes.string),
-    // list: PropTypes.array,
   }).isRequired,
 };
 
